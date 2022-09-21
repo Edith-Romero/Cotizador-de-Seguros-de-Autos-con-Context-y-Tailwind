e931d34f13f0429bd7ff5e18fa51ce24a5cfe82e
Edith Romero <edithjohanaromero@gmail.com>
2022-09-21 16:47:57 +0200

    Agregue componente de error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,29 +1,48 @@
-import { Fragment } from 'react';
-import { MARCAS,YEARS, PLANES } from '../constants';
-import  useCotizador from '../hooks/useCotizador'
+import { Fragment } from "react";
+import { MARCAS, YEARS, PLANES } from "../constants";
+import useCotizador from "../hooks/useCotizador";
+import Error from "./Error";
 
 const Formulario = () => {
+  const { datos, handleChangeDatos, error, setError } = useCotizador();
 
-  const {datos, handleChangeDatos } = useCotizador()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(datos).includes('')) {
+      setError("Todos los campos Obligatorios")
+      return;
+    }
+
+    setError('')
+
+    //TODO: Cotizar
+
+  };
 
   return (
     <>
-      <form>
+      {error && <Error />}
+      <form
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
-          <label className="block mb-3 font-bold text-gray-400 uppercase">MARCA</label>
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            MARCA
+          </label>
           <select
             name="marca"
             className="w-full p-3 bg-white border border-gray-200"
             value={datos.marca}
-            onChange={e => handleChangeDatos(e)} >
-            <option value = "">---Seleccione una Marca---</option>           
+            onChange={(e) => handleChangeDatos(e)}
+          >
+            <option value="">---Seleccione una Marca---</option>
             {/* Itera el arreglo con el .map y me coloca las opciones como dinamicas */}
-            {MARCAS.map(marca =>(
-              <option
-                key={marca.id}
-                value={marca.id}     
-              >{marca.nombre}</option>
-                ))}
+            {MARCAS.map((marca) => (
+              <option key={marca.id} value={marca.id}>
+                {marca.nombre}
+              </option>
+            ))}
           </select>
         </div>
 
@@ -34,44 +53,42 @@ const Formulario = () => {
           <select
             name="year"
             className="w-full p-3 bg-white border border-gray-200"
-            value = {datos.year}
-            onChange={e => handleChangeDatos(e)}      
-            >
-            <option value = "">---Seleccione el Año---</option>
-            
-            {YEARS.map(year=>(
-              <option
-                key={year}
-                value={year}
-              >{year}</option>
-                ))}
+            value={datos.year}
+            onChange={(e) => handleChangeDatos(e)}
+          >
+            <option value="">---Seleccione el Año---</option>
 
+            {YEARS.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
           </select>
         </div>
         <div className="my-5">
           <label className="block mb-3 font-bold text-gray-400 uppercase">
             Elige el plan
           </label>
-            <div className='flex gap-3 items-center'>
-            {PLANES.map(plan=>(
-                <Fragment key={plan.id}>
-                  <label>{plan.nombre}</label>
-                  <input
-                    type= "radio"
-                    name="plan"
-                    value={plan.id}
-                    onChange={e => handleChangeDatos(e)}      
-                  />
-                </Fragment>
-                ))}
-            </div>
+          <div className="flex gap-3 items-center">
+            {PLANES.map((plan) => (
+              <Fragment key={plan.id}>
+                <label>{plan.nombre}</label>
+                <input
+                  type="radio"
+                  name="plan"
+                  value={plan.id}
+                  onChange={(e) => handleChangeDatos(e)}
+                />
+              </Fragment>
+            ))}
+          </div>
         </div>
 
         <input
           type="submit"
           value="Cotizar"
-          className='w-full bg-indigo-500 hover:bg--indigo-600 transition-colors text-white cursor-pointer p-3 uppercase font-bold rounded-md'
-          onChange={e => handleChangeDatos(e)}      
+          className="w-full bg-indigo-500 hover:bg--indigo-600 transition-colors text-white cursor-pointer p-3 uppercase font-bold rounded-md"
+          onChange={(e) => handleChangeDatos(e)}
         />
       </form>
     </>
--- src/context/CotizadorProvaider.jsx
+++ src/context/CotizadorProvaider.jsx
@@ -1,5 +1,5 @@
 // 1.- Se importa el hooks
-import {useState, createContext} from 'react'
+import { useState, createContext } from "react";
 
 // 2.- Se guarda en una variable
 
@@ -7,36 +7,37 @@ const CotizadorContext = createContext();
 
 // 4.- Se pasa un children que seran los componentes hijos
 // del provaider es de donde salen los datos
-const CotizadorProvaider = ({children}) => {
-
+const CotizadorProvaider = ({ children }) => {
     const [datos, setDatos] = useState({
-        marca:"",
-        year:"",
-        plan:""
-    })
+        marca: "",
+        year: "",
+        plan: "",
+    });
 
-    const handleChangeDatos = e =>{
-        setDatos ({
+    const [error, setError] = useState("")
+
+    const handleChangeDatos = (e) => {
+        setDatos({
             ...datos,
-            [e.target.name] : e.target.value
-        })
-    }
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  return (
-    //3.- Se pasa como un componente con la anotacion de provaider
-    <CotizadorContext.Provider
-        value= {{
-            datos,
-            handleChangeDatos
-        }}
-    >
-        {children}
-    </CotizadorContext.Provider>
-  )
-}
+    return (
+        //3.- Se pasa como un componente con la anotacion de provaider
+        <CotizadorContext.Provider
+            value={{
+                datos,
+                handleChangeDatos,
+                error,
+                setError
+            }}
+        >
+            {children}
+        </CotizadorContext.Provider>
+    );
+};
 //5.- Aca se exporta por default la variable y se exporta normal la funcion tipo componente
-export{
-    CotizadorProvaider
-}
+export { CotizadorProvaider };
 
 export default CotizadorContext;
